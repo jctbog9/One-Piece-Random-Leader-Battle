b827f9501748bdb8e00fba26fba924d1a4678a18
John Trocchi <jtrocchi1@gmail.com>
2026-02-23 02:39:13 -0500

    import image for build process

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import sitePreviewImage from './assets/images/site-preview.png'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
